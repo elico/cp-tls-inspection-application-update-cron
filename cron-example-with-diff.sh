4d06b09490367a98896669525d895af107af0876
root <root@web-041.ngtech.home>
2021-03-10 19:15:52 +0200

    4

---
+++ cron-example-with-diff.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+DEBUG="1"
+DRY_RUN="0"
+
+APP_NAME="$1"
+
+URL="http://f-gogs.ngtech.home/NgTech-Home/tls-bypass-lists/raw/master/010-GeneralTLSInspectionBypass"
+
+if [ -z "${APP_NAME}" ];then
+        echo "Missing App Name"
+        exit 1
+fi
+
+if [ "$2" == "check" ];then
+        DRY_RUN="1"
+        echo "Running in dry run mode" >&2
+fi
+
+if [ ! -z "$3" ];then
+        URL="$3"
+fi
+
+TMP_DOWNLOAD_FILE=$(mktemp)
+
+wget "${URL}" -O ${TMP_DOWNLOAD_FILE}
+RES=$?
+
+if [ "${RES}" -gt "0" ];then
+        echo "Error Downloading file from URL: \"${URL}\""
+        rm -v "${TMP_DOWNLOAD_FILE}"
+        exit ${RES}
+fi
+
+TMP_CLISH_UPDATE_FILE=$( mktemp )
+
+TMP_CURRENT_CONFIG_FILE=$( mktemp )
+
+TMP_CLISH_TRANSACTION_FILE=$( mktemp )
+
+clish -c "show configuration"|egrep "^set application application-name \"${APP_NAME}\"" > ${TMP_CURRENT_CONFIG_FILE}
+
+while read line; do
+        if [ "${DEBUG}" -gt "0" ];then
+                echo -n "DEBUG LEVEL 1: Working on regex: " >&2
+                echo ${line} >&2
+        fi
+        echo "set application application-name \"${APP_NAME}\" regex-url true add url \"$line\"" >> ${TMP_CLISH_UPDATE_FILE}
+done < ${TMP_DOWNLOAD_FILE}
+
+DIFF=$(diff "${TMP_CURRENT_CONFIG_FILE}" "${TMP_CLISH_UPDATE_FILE}" )
+
+DELETE_OBJECTS=$(echo "${DIFF}" |egrep "^-set " |awk '{print $7}')
+
+for object in ${DELETE_OBJECTS}; do
+        echo "set application application-name \"${APP_NAME}\" remove url ${object}" >> ${TMP_CLISH_TRANSACTION_FILE}
+done
+
+echo "${DIFF}" |egrep "^\+set " |sed -e "s@^\+set @set @g" >>  ${TMP_CLISH_TRANSACTION_FILE}
+
+cat "${TMP_CLISH_TRANSACTION_FILE}"
+
+if [ "${DRY_RUN}" -eq "0" ];then
+        clish -f "${TMP_CLISH_TRANSACTION_FILE}"
+fi
+
+echo "Finished Transaction"
+echo "Cleaning up files ..."
+
+rm -v "${TMP_DOWNLOAD_FILE}"
+rm -v "${TMP_CLISH_UPDATE_FILE}"
+rm -v "${TMP_CURRENT_CONFIG_FILE}"
+rm -v "${TMP_CLISH_TRANSACTION_FILE}"
