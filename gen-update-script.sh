6a27ef5b9a26dc39e66e1ff6df2be0db3e6b05e2
Eliezer Croitoru <ngtech1ltd@gmail.com>
2021-07-14 16:17:50 +0300

    13

--- gen-update-script.sh
+++ gen-update-script.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_MD5_SUM=$(md5sum cron-example-with-diff-dstdom.sh|awk '{print $1}')
 SCRIPT_URL="https://raw.githubusercontent.com/elico/cp-tls-inspection-application-update-cron/master/cron-example-with-diff-dstdom.sh"
-DST_DOMAIN_LIST_URL="https://gist.githubusercontent.com/elico/249034a199d17ce52524f47fad49964f/raw/bdd95d87232f8173185acc14540d58bfb2c9ff79/010-GeneralTLSInspectionBypass.dstdom"
+DST_DOMAIN_LIST_URL=$(head -1 dst-domain-url)
 APP_NAME="NgTechBypassDstDomain"
 SCRIPT_PATH="/storage/cron-example-with-diff-dstdom.sh"
 
